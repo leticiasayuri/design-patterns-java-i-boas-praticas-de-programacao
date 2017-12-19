60b5067e0edb2f673cd42ba8da224fddeb8dbaa9
Leticia Sayuri <leticiasyrk@gmail.com>
2017-12-18 23:57:50 -0200

    [Aula 02] Classes Orcamento e Item.

---
+++ corrente-de-descontos/src/com/alura/correntededescontos/Orcamento.java
@@ -0,0 +1,37 @@
+package com.alura.correntededescontos;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Orcamento {
+	
+	private double valor;
+	
+	private List<Item> itens;
+	
+	public Orcamento(double valor) {
+		setValor(valor);
+		this.itens = new ArrayList<>();
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public List<Item> getItens() {
+		return Collections.unmodifiableList(itens);
+	}
+
+	public void setItens(List<Item> itens) {
+		this.itens = itens;
+	}
+	
+	public void adicionaItem(Item item) {
+		itens.add(item);
+	}
+}
